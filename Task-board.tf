4a5d6f0ffd82e37cf00da91da8148db7a1ec153c
DimAndAnd <123008948+DimAndAnd@users.noreply.github.com>
2024-12-07 15:50:48 +0200

    update Task-board terraform file

--- Task-board.tf
+++ Task-board.tf
@@ -5,6 +5,12 @@ terraform {
       version = "4.12.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "StorageRG"
+    storage_account_name = "dianderstoragetb"
+    container_name       = "storagetaskboard"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
